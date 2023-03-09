7fad7277b9c543cc7bab48b81d0ffacff489e097
qiliang <ql.sun@joyson.cn>
2023-03-09 16:14:29 +0800

    update

--- TypicalExample/ros2com/install/fastdds_record_typesupport/share/fastdds_record_typesupport/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(fastdds_record_typesupport_IDL_FILES "idl/HelloWorld.idl")
-set(fastdds_record_typesupport_INTERFACE_FILES "")
